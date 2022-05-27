7abb7c0a710f4e71020948c790b3c1bd7938308c
cydneysmith <cydney.smith@kellogg.northwestern.edu>
2022-05-27 17:58:27 +0000

    getting stuck

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,27 @@ class SessionsController < ApplicationController
   end
   
   def create
-    # TODO: authenticate user
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+        # login the user
+        session["user_id"]= @user["id"]
+        flash["notice"] = "You've logged in."
+        redirect_to "/posts"
+      else
+        flash["notice"] = "Unsuccessful login."
+        redirect_to "/login"
+      end
+      else
+        flash["notice"] = "Unsuccessful login."
+        redirect_to "/login"
+        # TODO: authenticate user
+      end
+  end
+
+  def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
 end
